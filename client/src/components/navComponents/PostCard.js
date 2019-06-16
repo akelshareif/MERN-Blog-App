1c6dd3110967af2006ab447851972ba7882b2707
akelshareif <akelshareif@gmail.com>
2019-06-16 15:16:15 -0400

    modified db with new cluster due to connection issues

--- client/src/components/navComponents/PostCard.js
+++ client/src/components/navComponents/PostCard.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { Card } from 'semantic-ui-react';
-import { NavLink } from 'react-router-dom';
 import '../../styles/styles.css';
 
 
 // this component must receive prop values to dynamically update
 // once card is built with new-post data, it must be sent to <StackContainer /> component for rendering
+// have card open a new pop-up component when clicked 
 
 const PostCard = props => {
 
+    const handleClick = () => {
+        console.log("clicked");
+    }
+   
     return(
+
         <Card
             link
-            as={NavLink}
-            exact
-            to='/new/id'
+            onClick={handleClick}
             image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkM_YGRfI5olnPa5XmGJeDhu5etD9ZrD3PwCz4hmfWF6cPxUACoQ'
             header='Test Card'
             meta='Posted on'
---
+++ keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://shariffMonger:ifw2emvFGv5JV5S5@posts-czz7k.mongodb.net/test?retryWrites=true&w=majority'
+}
